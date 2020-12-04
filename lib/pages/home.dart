485612482755b9361e4b04490a67d6e53196dcd1
OLAWALE <yussupholawale2017@gmail.com>
2020-12-04 15:07:59 +0100

    change image

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,18 +3,17 @@ import 'package:flutter/material.dart';
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
-
 }
 
 class _HomeState extends State<Home> {
   Map data = {};
   @override
   Widget build(BuildContext context) {
-    data = data.isNotEmpty ? data : ModalRoute.of( context).settings.arguments;
+    data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
     print(data);
 
     //set background
-    String bgImage = data['isDayTime'] ? 'day.png' : 'night.png';
+    String bgImage = data['isDayTime'] ? 'day.jpg' : 'night.jpg';
     Color bgColor = data['isDayTime'] ? Colors.blue : Colors.indigo[700];
     Color iconColor = data['isDayTime'] ? Colors.blue[400] : Colors.blue[800];
 
@@ -35,29 +34,28 @@ class _HomeState extends State<Home> {
                 FlatButton.icon(
                   color: iconColor,
                   onPressed: () async {
-                   dynamic result = await Navigator.pushNamed(context, '/location');
-                   setState(() {
-                     data = {
-                       'time': result['time'],
-                       'location': result['location'],
-                       'isDayTime': result['isDayTime'],
-                       'flag': result['flag']
-                     };
-                   });
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/location');
+                    setState(() {
+                      data = {
+                        'time': result['time'],
+                        'location': result['location'],
+                        'isDayTime': result['isDayTime'],
+                        'flag': result['flag']
+                      };
+                    });
                   },
                   icon: Icon(
-                      Icons.edit_location,
-                      color: Colors.grey[300],
+                    Icons.edit_location,
+                    color: Colors.grey[300],
                   ),
-                  label: Text(
-                      'Edit location',
-                        style: TextStyle(
-                      color: Colors.white,
-                  )
-                  ),
+                  label: Text('Edit location',
+                      style: TextStyle(
+                        color: Colors.white,
+                      )),
                 ),
                 SizedBox(height: 20),
-               Row(
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     CircleAvatar(
